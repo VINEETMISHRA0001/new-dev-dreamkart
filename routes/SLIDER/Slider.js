e04d6c90cbc02417d65c1ee502bab96313535394
Vineet Mishra <02vineetmishra@gmail.com>
2024-12-20 13:34:11 +0530

    dasdsad

--- routes/SLIDER/Slider.js
+++ routes/SLIDER/Slider.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const upload = require('../../middlewares/MULTER/newMulter');
 const sliderController = require('./../../controllers/SLIDERS/SliderController');
 
-router.post('/', upload.single('image'), sliderController.createSlider); // Upload slider with image
+router.post('/', upload.array('image', 1), sliderController.createSlider); // Upload slider with image
 router.get('/', sliderController.getSliders); // Get all sliders
-router.put('/:id', upload.single('image'), sliderController.updateSlider); // Update slider and image
+router.put('/:id', upload.array('image', 1), sliderController.updateSlider); // Update slider and image
 router.delete('/:id', sliderController.deleteSlider); // Delete slider
 
 module.exports = router;
